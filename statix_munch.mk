13a09c6dc240bb0593da40f357424a956f0bf734
yoriscape <sukhpreetsingh58627@gmail.com>
2024-06-03 19:36:59 +0000

    munch: init StatixOS

--- statix_munch.mk
+++ statix_munch.mk
@@ -8,15 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-DEVICE_MAINTAINER := madmax7896
+INCLUDE_PIXEL_LAUNCHER := true
 
-PRODUCT_NAME := arrow_munch
+PRODUCT_NAME := statix_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
